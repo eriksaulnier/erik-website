227976c6d87d5c440e03b7e53b3dae38d4333605
Erik Saulnier <me@eriksaulnier.com>
2020-10-12 16:06:39 -0400

    Styled 404 page

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import { Layout } from '../components/layout'
 export default function Custom404({ site }) {
   return (
     <Layout site={site} pageTitle="404">
-      <h1>404 - Page Not Found</h1>
+      <h1 style={{ textAlign: 'center', marginTop: '10%' }}>Page Not Found</h1>
     </Layout>
   )
 }
