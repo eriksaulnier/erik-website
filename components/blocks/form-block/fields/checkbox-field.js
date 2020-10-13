80d6edb4e605e54da51826c040e117aee5e7ad2e
Erik Saulnier <me@eriksaulnier.com>
2020-10-13 18:08:11 -0400

    Some accessiblity fixes

--- components/blocks/form-block/fields/checkbox-field.js
+++ components/blocks/form-block/fields/checkbox-field.js
@@ -2,8 +2,8 @@ import styles from './../form-block.module.scss'
 
 export default function CheckboxField({ data }) {
   return (
-    <div className={[styles.Field, styles.CheckboxField].join(' ')} width={data.width||100}>
-      {data.label ? <label htmlFor={data.name}>{data.label}</label> : null}
+    <fieldset className={[styles.Field, styles.CheckboxField].join(' ')} width={data.width||100}>
+      {data.label ? <legend htmlFor={data.name}>{data.label}</legend> : null}
       {data.checkbox_options.map((option, index) => (
         <div key={index}>
           <input id={data.name} type="checkbox" name={data.name+'[]'} value={option.value} />
@@ -11,6 +11,6 @@ export default function CheckboxField({ data }) {
         </div>
       ))}
       {data.description ? <p className={styles.Description}>{data.description}</p> : null}
-    </div>
+    </fieldset>
   )
 }
--- components/blocks/form-block/fields/radio-field.js
+++ components/blocks/form-block/fields/radio-field.js
@@ -2,15 +2,15 @@ import styles from './../form-block.module.scss'
 
 export default function RadioField({ data }) {
   return (
-    <div className={[styles.Field, styles.RadioField].join(' ')} width={data.width || 100}>
-      {data.label ? <label htmlFor={data.name}>{data.label}</label> : null}
+    <fieldset className={[styles.Field, styles.RadioField].join(' ')} width={data.width || 100}>
+      {data.label ? <legend>{data.label}</legend> : null}
       {data.radio_options.map((option, index) => (
         <div key={index}>
           <input id={data.name} type="radio" name={data.name} value={option.value}/>
-          <label htmlFor={option.value}>{option.label}</label>
+          <label htmlFor={option.name}>{option.label}</label>
         </div>
       ))}
       {data.description ? <p className={styles.Description}>{data.description}</p> : null}
-    </div>
+    </fieldset>
   )
 }
