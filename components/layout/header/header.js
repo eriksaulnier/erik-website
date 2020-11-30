8e04c88a79b42339ba82039e5aacb7b8bbec250d
Erik Saulnier <me@eriksaulnier.com>
2020-11-29 22:33:36 -0500

    Made header styles mobile-first

--- components/layout/header/header.js
+++ components/layout/header/header.js
@@ -32,6 +32,16 @@ export default function Header({ siteTitle, navigation, social_links }) {
     }
   }, [])
 
+  // Prevent scrolling when the menu is open
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden'
+      window.scrollTo(0, 0)
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [open])
+
   function onMenuMouseEnter(path) {
     if (!navigating) {
       setCurrentPath(path)
@@ -49,21 +59,31 @@ export default function Header({ siteTitle, navigation, social_links }) {
 
   const activeMenuItem = navigation.find(page => `/${page.slug}` === currentPath)
 
-  const navigationVariants = {
-    open: { height: '100%' },
-    closed: { height: '40px' }
+  const menuVariants = {
+    open: {
+      display: 'flex',
+      opacity: 1,
+      transition: {
+        type: 'linear',
+        duration: 0.3
+      }
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        type: 'linear',
+        duration: 0.3
+      },
+      transitionEnd: {
+        display: 'none'
+      }
+    }
   }
 
   return (
     <nav className={styles.header}>
       <div className={styles.container}>
-        <motion.nav
-          initial={false}
-          animate={open ? 'open' : 'closed'}
-          variants={navigationVariants}
-          transition={{ ease: 'linear' }}
-          className={[styles.navigation, open && styles.active].join(' ')}
-        >
+        <nav className={[styles.navigation, open && styles.active].join(' ')}>
           <Link href="/">
             <a
               className={styles.brand}
@@ -75,7 +95,12 @@ export default function Header({ siteTitle, navigation, social_links }) {
             </a>
           </Link>
 
-          <button className={styles.menuToggle} onClick={() => setOpen(!open)}>
+          <motion.button
+            initial={false}
+            animate={open ? 'open' : 'closed'}
+            className={styles.menuToggle}
+            onClick={() => setOpen(!open)}
+          >
             <svg viewBox="0 0 20 20">
               <Path
                 variants={{
@@ -95,37 +120,45 @@ export default function Header({ siteTitle, navigation, social_links }) {
                 }}
               />
             </svg>
-          </button>
-
-          <div className={styles.menu}>
-            {navigation && navigation.map((page, index) => (
-              <div key={index} className={styles.menuItem}>
-                <Link href={'/[...slug]'} as={`/${page.slug}`}>
-                  <a
-                    onMouseEnter={() => onMenuMouseEnter(`/${page.slug}`)}
-                    onMouseLeave={onMenuMouseLeave}
-                  >
-                    {page.label}
-                    {MenuHighlight(currentPath === `/${page.slug}`)}
-                  </a>
-                </Link>
-              </div>
-            ))}
-          </div>
+          </motion.button>
           
-          <div className={styles.social}>
-            {social_links ? social_links.map((item, index) => (
-              <div key={index} className={styles.socialIcon}>
-                <Link href={item.link}>
-                  <a target="_blank" rel="noopener">
-                    <span className="sr-only">{item.name}</span>
-                    {item.icon ? React.createElement(Fa[item.icon]) : null}
-                  </a>
-                </Link>
-              </div>
-            )) : null}
-          </div>
-        </motion.nav>
+          <motion.div 
+            initial={false}
+            animate={open ? 'open' : 'closed'}
+            variants={menuVariants}
+            transition={{ ease: 'linear' }}
+            className={styles.menu}
+          >
+            <div className={styles.menuItems}>
+              {navigation && navigation.map((page, index) => (
+                <div key={index} className={styles.menuItem}>
+                  <Link href={'/[...slug]'} as={`/${page.slug}`}>
+                    <a
+                      onMouseEnter={() => onMenuMouseEnter(`/${page.slug}`)}
+                      onMouseLeave={onMenuMouseLeave}
+                    >
+                      {page.label}
+                      {MenuHighlight(currentPath === `/${page.slug}`)}
+                    </a>
+                  </Link>
+                </div>
+              ))}
+            </div>
+            
+            <div className={styles.social}>
+              {social_links ? social_links.map((item, index) => (
+                <div key={index} className={styles.socialIcon}>
+                  <Link href={item.link}>
+                    <a target="_blank" rel="noopener">
+                      <span className="sr-only">{item.name}</span>
+                      {item.icon ? React.createElement(Fa[item.icon]) : null}
+                    </a>
+                  </Link>
+                </div>
+              )) : null}
+            </div>
+          </motion.div>
+        </nav>
       </div>
     </nav>
   )
