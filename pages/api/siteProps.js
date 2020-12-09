0f0053a56db8fb38454f05df51ad6d55f5bd1cfc
Erik Saulnier <me@eriksaulnier.com>
2020-12-08 21:31:36 -0500

    Removed silly markdown API

--- pages/api/siteProps.js
+++
@@ -1,9 +0,0 @@
-import { getSiteProps } from '../../lib/api'
-
-export default function handler(req, res) {
-  const siteProps = getSiteProps()
-
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(siteProps))
-}
