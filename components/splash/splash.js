18e0095346b1f23d13894f720190274bf116b53c
Erik Saulnier <me@eriksaulnier.com>
2020-12-16 22:51:15 -0500

    Fixed animation glitch when navigating from the splash

--- components/splash/splash.js
+++ components/splash/splash.js
@@ -1,24 +1,13 @@
 import { motion } from 'framer-motion'
 import styles from './splash.module.scss'
-import { AnimatedText } from '../animations'
 
 export default function Splash() {
-  const pageAnimations = {
-    initial: { opacity: 0 },
-    enter: { opacity: 1, transition: { staggerChildren: 0.3 } },
-    exit: { opacity: 0 }
-  }
-
   const title = "Hey, I'm Erik!"
   const titleParts = [...title]
   const subtitle = "I'm a full-stack developer who loves turning ideas into code."
   
   return (
-    <motion.div
-      className={styles.splash}
-      initial="initial" animate="enter" exit="exit"
-      variants={pageAnimations}
-    >
+    <div className={styles.splash}>
       <div className={styles.centered}>
         <motion.h1
           className={styles.title}
@@ -33,7 +22,6 @@ export default function Splash() {
               variants={{
                 initial: { opacity: 0, y: 15, scaleX: 0.8 },
                 enter: { opacity: 1, y: 0, scaleX: 1 },
-                exit: { opacity: 0, y: 15 },
                 hover: { y: -5, scaleX: 0.9 }
               }}
               whileHover="hover"
@@ -52,7 +40,6 @@ export default function Splash() {
           variants={{
             initial: { opacity: 0, y: 15 },
             enter: { opacity: 1, y: 0 },
-            exit: { opacity: 0 }
           }}
           transition={{
             delay: 0.13 * title.length,
@@ -63,6 +50,6 @@ export default function Splash() {
           {subtitle}
         </motion.h2>
       </div>
-    </motion.div>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { motion } from 'framer-motion'
 import Splash from '../components/splash';
 
 export default function Home() {
   return (
-    <Splash />
+    <motion.div
+      initial="initial" animate="enter" exit="exit"
+      variants={{ enter: { transition: { staggerChildren: 0.15 } } }}
+    >
+      <Splash />
+    </motion.div>
   )
 }
