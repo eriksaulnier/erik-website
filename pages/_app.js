6b66764ff9aca9ec5851409e7d7731a007c58044
Erik Saulnier <me@eriksaulnier.com>
2020-12-19 16:53:38 -0500

    Temporary fix for the transition glitch

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { Router } from 'next/router'
 import { AnimateSharedLayout, AnimatePresence } from 'framer-motion'
 import { getSiteConfig } from '../lib/api'
 import { Layout } from '../components/layout'
@@ -5,6 +7,26 @@ import '../styles/globals.scss'
 
 export default function MyApp({ Component, pageProps, router }) {
   const siteConfig = getSiteConfig();
+
+  useState(() => {
+    const routeChange = () => {
+      // Temporary fix to avoid flash of unstyled content
+      // during route transitions. Keep an eye on this
+      // issue and remove this code when resolved:
+      // https://github.com/vercel/next.js/issues/17464
+
+      const tempFix = () => {
+        const allStyleElems = document.querySelectorAll('style[media="x"]')
+        allStyleElems.forEach((elem) => {
+          elem.removeAttribute('media')
+        })
+      }
+      tempFix()
+    }
+
+    Router.events.on('routeChangeComplete', routeChange)
+    Router.events.on('routeChangeStart', routeChange)
+  }, [])
 
   return (
     <AnimateSharedLayout>
