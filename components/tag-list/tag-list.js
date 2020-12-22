571c95c24964552a2fa597183f8011befe99eef9
Erik Saulnier <me@eriksaulnier.com>
2020-12-21 22:49:48 -0500

    Added icons to the TagList component

--- components/tag-list/tag-list.js
+++ components/tag-list/tag-list.js
@@ -1,6 +1,5 @@
-// import React from 'react'
-// import * as Devicon from 'react-icons/di' 
 import { motion } from 'framer-motion'
+import Icon from '../icon'
 import styles from './tag-list.module.scss'
 
 export default function TagList({ tags, color }) {
@@ -15,17 +14,22 @@ export default function TagList({ tags, color }) {
         <motion.li
           key={index}
           className={[styles.tag, color ? styles[color] : null].join(' ')}
+          style={{ cursor: 'pointer' }}
           variants={{
             initial: { opacity: 0, x: 10 },
             enter: { opacity: 1, x: 0 },
-            exit: { opacity: 0 }
+            exit: { opacity: 0 },
+            hover: { y: -5 }
           }}
+          whileHover="hover"
           transition={{
             type: 'spring',
             stiffness: 150
           }}
+          drag
+          dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
         >
-          {/* {tag.icon ? React.createElement(Devicon[tag.icon]) : null} */}
+          {tag.icon_name && <Icon name={tag.icon_name} />}
           {tag.label}
         </motion.li>
       ))}
