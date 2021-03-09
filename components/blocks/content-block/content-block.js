afe08d827901387c0f08eb2a22c754889b1e1e5c
Erik Saulnier <me@eriksaulnier.com>
2021-03-08 23:50:29 -0500

    Added support for an aside image in the content block

--- components/blocks/content-block/content-block.js
+++ components/blocks/content-block/content-block.js
@@ -1,25 +1,36 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import ReactMarkdown from 'react-markdown/with-html'
+import styles from './content-block.module.scss'
 
-export default function ContentBlock({ data: { block_title, content }}) {
+export default function ContentBlock({ data: { block_title, content, aside_image }}) {
   return (
-    <section>
-      {block_title && <h2>{block_title}</h2>}
-      
+    <section className={styles.contentBlock}>
       {content && (
-        <ReactMarkdown
-          source={content}
-          escapeHtml={false}
-          renderers={{
-            link: ({ children, href }) => {
-              return (
-                <Link href={href}>
-                  <a>{children}</a>
-                </Link>
-              );
-            },
-          }}
-        />
+        <div className={styles.content}>
+          {block_title && <h2>{block_title}</h2>}
+
+          <ReactMarkdown
+            source={content}
+            escapeHtml={false}
+            className={styles.content}
+            renderers={{
+              link: ({ children, href }) => {
+                return (
+                  <Link href={href}>
+                    <a>{children}</a>
+                  </Link>
+                );
+              },
+            }}
+          />
+        </div>
+      )}
+
+      {aside_image && (
+        <div className={styles.asideImage}>
+          <Image src={aside_image} width="500" height="500" objectFit="cover" />
+        </div>
       )}
     </section>
   )
