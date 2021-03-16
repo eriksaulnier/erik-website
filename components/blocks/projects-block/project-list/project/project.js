7cad3bdd0f07403f90f6fcf2a60e008a95bc04d5
Erik Saulnier <me@eriksaulnier.com>
2021-03-15 21:45:16 -0400

    Apparently I put the description styles on the date

--- components/blocks/projects-block/project-list/project/project.js
+++ components/blocks/projects-block/project-list/project/project.js
@@ -11,6 +11,7 @@ export default function Project({ data }) {
   return (
     <motion.div
       className={styles.project}
+      whileHover="hover"
       variants={{
         initial: { opacity: 0, y: 10 },
         enter: { opacity: 1, y: 0 }
@@ -21,7 +22,7 @@ export default function Project({ data }) {
       }}
     >
       <div className={styles.image}>
-        <motion.div whileHover="hover" variants={{ initial: { opacity: 0.4 }, hover: { opacity: 1 } }}>
+        <motion.div variants={{ initial: { opacity: 0.4 }, hover: { opacity: 1 } }}>
           <Image alt={data.title} src={data.thumbnail} width="525" height="350" />
         </motion.div>
       </div>
