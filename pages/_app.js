8de2dcb16a134eb9300ad3885ce4d878e031ba7c
Erik Saulnier <me@eriksaulnier.com>
2021-05-09 20:59:16 -0400

    Trying an updated fix for #1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { Router } from 'next/router'
+import { useEffect } from 'react'
 import { AnimateSharedLayout, AnimatePresence } from 'framer-motion'
 import { getSiteConfig } from '@/lib/api'
 import { Layout } from '@/components/layout'
@@ -8,30 +7,35 @@ import '@/styles/globals.scss'
 export default function MyApp({ Component, pageProps, router }) {
   const siteConfig = getSiteConfig()
 
-  useState(() => {
-    // Temporary fix to avoid flash of unstyled content
-    // during route transitions. Keep an eye on this
-    // issue and remove this code when resolved:
-    // https://github.com/vercel/next.js/issues/17464
-
-    const routeChange = () => {
-      const tempFix = () => {
-        const allStyleElems = document.querySelectorAll('style[media="x"]')
-        allStyleElems.forEach((elem) => {
-          elem.removeAttribute('media')
-        })
-      }
-      tempFix()
-    }
-
-    Router.events.on('routeChangeComplete', routeChange)
-    Router.events.on('routeChangeStart', routeChange)
-
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+  useEffect(() => {
+    Array.from(
+      document.querySelectorAll('head > link[rel="stylesheet"][data-n-p]')
+    ).forEach(node => {
+      node.removeAttribute('data-n-p');
+    });
+    const mutationHandler = mutations => {
+      mutations.forEach(({ target }) => {
+        if (target.nodeName === 'STYLE') {
+          if (target.getAttribute('media') === 'x') {
+            target.removeAttribute('media');
+          }
+        }
+      });
+    };
+    
+    const observer = new MutationObserver(mutationHandler);
+    observer.observe(document.head, {
+      subtree: true,
+      attributeFilter: ['media'],
+    });
     return () => {
-      Router.events.off('routeChangeComplete', routeChange)
-      Router.events.off('routeChangeStart', routeChange)
-    }
-  }, [])
+      observer.disconnect();
+    };
+  }, []);
 
   return (
     <AnimateSharedLayout>
