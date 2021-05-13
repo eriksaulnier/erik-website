746cb49a7f02e04cb85fd5a8185b804b6b7b7b0d
Erik Saulnier <me@eriksaulnier.com>
2021-05-12 20:07:07 -0400

    Set up build analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = {
+module.exports = withBundleAnalyzer({
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -14,4 +17,4 @@ module.exports = {
 
     return config
   }
-}
+})
