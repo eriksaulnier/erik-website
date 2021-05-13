2f2cc89ba0167c3d769f232df977452559497d63
Erik Saulnier <me@eriksaulnier.com>
2021-05-12 20:09:09 -0400

    Forgot to make it a string

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  target: serverless,
+  target: 'serverless',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
