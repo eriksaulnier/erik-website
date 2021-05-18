666549fa923f45736b10ab07bffadd839955f8c8
Erik Saulnier <me@eriksaulnier.com>
2021-05-18 18:29:47 -0400

    Updated site description to be fillable using Forestry

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -24,28 +24,26 @@ export default function Layout({ siteConfig, pageTitle, children }) {
         <meta name="msapplication-TileColor" content="#FFFFFF"/>
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
         <meta name="theme-color" content="#FFFFFF"/>
-        <meta
+        {siteConfig.description && <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
+          content={siteConfig.description}
+        />}
+        {siteConfig.thumbnail_image && <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteConfig.site_title
-          )}.png?theme=dark&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteConfig.site_title} />
+          content={siteConfig.thumbnail_image}
+        />}
+        <meta name="og:title" content={siteConfig.title} />
         <meta name="twitter:card" content="summary_large_image" />
-        <title>{pageTitle ? `${pageTitle} | ${siteConfig.site_title}` : siteConfig.site_title}</title>
+        <title>{pageTitle ? `${pageTitle} | ${siteConfig.title}` : siteConfig.title}</title>
       </Head>
 
-      <Header siteTitle={siteConfig.site_title} {...siteConfig.header} />
+      <Header siteTitle={siteConfig.title} {...siteConfig.header} />
       <main className={styles.content}>
         <div className={styles.container}>
           {children}
         </div>
       </main>
-      <Footer layout siteTitle={siteConfig.site_title} {...siteConfig.footer} />
+      <Footer layout siteTitle={siteConfig.title} {...siteConfig.footer} />
     </div>
   )
 }
