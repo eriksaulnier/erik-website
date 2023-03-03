98da4ed6dffb8c3963f13602977260182047ba57
Erik Saulnier <me@eriksaulnier.com>
2023-03-03 10:57:54 -0500

    Updated icon to use Next Dynamic

--- components/icon.js
+++ components/icon.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import loadable from '@loadable/component'
+import dynamic from 'next/dynamic' 
 
 export default function DynamicIcon({ name, ...props }) {
   const family = name.slice(0, 2).toLowerCase()
-  const Icon = loadable(() => import(`react-icons/${family}/index.js`), {
-    resolveComponent: (el) => {
-      if (Object.assign({}, el).hasOwnProperty(name)) {
-        return el[name]
-      } else {
-        console.log('Failed to load icon:', name)
-        return React.Fragment
-      }
-    }
-  })
+  const Icon = dynamic(
+    () => import(`react-icons/${family}/index.js`).then((mod) => {
+      if (mod[name]) return mod[name]
+      throw new Error(`Icon ${name} not found`)
+    })
+  )
   
   return <Icon {...props} />
 }
