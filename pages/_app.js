17588bf151211924c5f8f01db018fb4c361720ac
Erik Saulnier <me@eriksaulnier.com>
2023-03-03 10:55:05 -0500

    Updated flashing styles fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,37 +4,71 @@ import { Layout } from '@/components/layout'
 import appConfig from '@/appConfig'
 import '@/styles/globals.scss'
 
-export default function MyApp({ Component, pageProps, router }) {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
+// TODO: Temporary fix to avoid flash of unstyled content during route
+// transitions. Keep an eye on this issue and remove this code when resolved:
+// https://github.com/vercel/next.js/issues/17464
+export const useNextCssRemovalPrevention = () => {
   useEffect(() => {
-    Array.from(
-      document.querySelectorAll('head > link[rel="stylesheet"][data-n-p]')
-    ).forEach(node => {
-      node.removeAttribute('data-n-p');
+    // Remove data-n-p attribute from all link nodes.
+    // This prevents Next.js from removing server rendered stylesheets.
+    document.querySelectorAll('head > link[data-n-p]').forEach(linkNode => {
+      linkNode.removeAttribute('data-n-p');
     });
-    const mutationHandler = mutations => {
-      mutations.forEach(({ target }) => {
-        if (target.nodeName === 'STYLE') {
-          if (target.getAttribute('media') === 'x') {
-            target.removeAttribute('media');
+
+    const mutationHandler = (mutations) => {
+      mutations.forEach(({ target, addedNodes }) => {
+        if (target.nodeName === 'HEAD') {
+          // Add data-n-href-perm attribute to all style nodes with attribute data-n-href,
+          // and remove data-n-href and media attributes from those nodes.
+          // This prevents Next.js from removing or disabling dynamic stylesheets.
+          addedNodes.forEach(node => {
+            const el = node;
+            if (el.nodeName === 'STYLE' && el.hasAttribute('data-n-href')) {
+              const href = el.getAttribute('data-n-href');
+              if (href) {
+                el.setAttribute('data-n-href-perm', href);
+                el.removeAttribute('data-n-href');
+                el.removeAttribute('media');
+              }
+            }
+          });
+
+          // Remove all stylesheets that we don't need anymore
+          // (all except the two that were most recently added).
+          const styleNodes = document.querySelectorAll('head > style[data-n-href-perm]');
+          const requiredHrefs = new Set();
+          for (let i = styleNodes.length - 1; i >= 0; i--) {
+            const el = styleNodes[i];
+            if (requiredHrefs.size < 2) {
+              const href = el.getAttribute('data-n-href-perm');
+              if (href) {
+                if (requiredHrefs.has(href)) {
+                  el.parentNode.removeChild(el);
+                } else {
+                  requiredHrefs.add(href);
+                }
+              }
+            } else {
+              el.parentNode.removeChild(el);
+            }
           }
         }
       });
     };
-    
+
+    // Observe changes to the head element and its descendents.
     const observer = new MutationObserver(mutationHandler);
-    observer.observe(document.head, {
-      subtree: true,
-      attributeFilter: ['media'],
-    });
+    observer.observe(document.head, { childList: true, subtree: true });
+
     return () => {
+      // Disconnect the observer when the component unmounts.
       observer.disconnect();
     };
   }, []);
+};
 
+export default function MyApp({ Component, pageProps, router }) {
+  useNextCssRemovalPrevention();
   return (
     <AnimateSharedLayout>
       <Layout appConfig={appConfig} pageTitle={pageProps?.tab_title || null}>
